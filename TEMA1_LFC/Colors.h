6b2c8f8b8667b8d04bd433b65b0827f9c1a96861
Cristian <cristian.ciulica@student.unitbv.ro>
2025-11-27 13:03:16 +0200

    Cleaned up Colors.h explicit comments

--- TEMA1_LFC/Colors.h
+++ TEMA1_LFC/Colors.h
@@ -1,34 +1,11 @@
 #pragma once
 
-// === PALETA MODERNA (Dracula / Cyberpunk Theme) ===
-
-// Resetare standard
 #define RESET   "\033[0m"
-
-// 1. Meniu & Titluri (Un Mov-Lavand? placut, in loc de albastru strident)
-// Cod RGB: 189, 147, 249
 #define CYAN    "\033[38;2;189;147;249m" 
-
-// 2. Input / Atentionari (Un Galben-Auriu pal)
-// Cod RGB: 241, 250, 140
 #define YELLOW  "\033[38;2;241;250;140m"
-
-// 3. Succes / Validare (Un Verde-Ment? vibrant)
-// Cod RGB: 80, 250, 123
 #define GREEN   "\033[38;2;80;250;123m"
-
-// 4. Erori (Un Ro?u-Coral, nu rosu sangeriu)
-// Cod RGB: 255, 85, 85
 #define RED     "\033[38;2;255;85;85m"
-
-// 5. Text Secundar / Delimitatori (Un Albastru-Gheata deschis)
-// Cod RGB: 139, 233, 253
 #define BLUE    "\033[38;2;139;233;253m"
-
-// 6. Mesaje de iesire (Roz Neon)
-// Cod RGB: 255, 121, 198
 #define MAGENTA "\033[38;2;255;121;198m"
-
-// 7. Text evidentiat (Alb stralucitor)
 #define BOLD    "\033[1m"
 #define WHITE   "\033[37m"
